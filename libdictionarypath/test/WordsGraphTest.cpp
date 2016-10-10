c55dc747a8426bc01dea09a7bff85f0e66fdf737
Grzegorz Rynkowski <grzegorz.rynkowski@gmail.com>
2016-10-10 20:38:02 +0100

    Correct and extend unit tests in WordsGraphTest

--- libdictionarypath/test/WordsGraphTest.cpp
+++ libdictionarypath/test/WordsGraphTest.cpp
@@ -6,47 +6,68 @@
 
 #include <gtest/gtest.h>
 
-#include "DictionaryPath/Dictionary.h"                     // DictionaryPtr
-#include "DictionaryPath/DictionaryFactory.h"              // DictionaryFactory
 #include "WordsGraphFactory.h"                             // WordsGraphFactory
 
 #include "MockDictionary.h"                                // MockDictionary
 
-TEST(AmountOfWords, NoWords)
+TEST(ProcessingDictionary, 0Words)
 {
     auto dict = MockDictionary::Create();
     auto wordsGraph = WordsGraphFactory::CreateWordsGraph();
     wordsGraph->setData(dict);
 
     auto nodes = wordsGraph->getNodes();
-    ASSERT_EQ(0, nodes->size());
+    EXPECT_EQ(0, nodes->size());
 }
 
-TEST(AmountOfWords, OneWord)
+TEST(ProcessingDictionary, 1Word)
 {
     auto dict = MockDictionary::Create("cat");
     auto wordsGraph = WordsGraphFactory::CreateWordsGraph();
     wordsGraph->setData(dict);
-    ASSERT_EQ(1, wordsGraph->getNodes()->size());
+    EXPECT_EQ(1, wordsGraph->getNodes()->size());
+    EXPECT_EQ(0, wordsGraph->getNeighbours("cat").size());
 }
 
-TEST(AmountOfWords, DifferentWords2)
+TEST(ProcessingDictionary, 2DifferentWords)
 {
     auto dict = MockDictionary::Create("cad", "cat");
     auto wordsGraph = WordsGraphFactory::CreateWordsGraph();
     wordsGraph->setData(dict);
-    ASSERT_EQ(2, wordsGraph->getNodes()->size());
+    EXPECT_EQ(2, wordsGraph->getNodes()->size());
+    EXPECT_EQ(1, wordsGraph->getNeighbours("cad").size());
+    EXPECT_EQ(1, wordsGraph->getNeighbours("cat").size());
+    EXPECT_EQ("cat", *wordsGraph->getNeighbours("cad").begin());
+    EXPECT_EQ("cad", *wordsGraph->getNeighbours("cat").begin());
 }
 
-TEST(AmountOfWords, SameWords2)
+TEST(ProcessingDictionary, 2SameWords)
 {
     auto dict = MockDictionary::Create("cat", "cat");
     auto wordsGraph = WordsGraphFactory::CreateWordsGraph();
     wordsGraph->setData(dict);
-    ASSERT_EQ(1, wordsGraph->getNodes()->size());
+    EXPECT_EQ(1, wordsGraph->getNodes()->size());
+    EXPECT_EQ(0, wordsGraph->getNeighbours("cat").size());
 }
 
-TEST(AmountOfWords, DifferentWords676)
+TEST(ProcessingDictionary, 3DifferentWords)
+{
+    auto dict = MockDictionary::Create("cad", "cat", "rat");
+    auto wordsGraph = WordsGraphFactory::CreateWordsGraph();
+    wordsGraph->setData(dict);
+    EXPECT_EQ(3, wordsGraph->getNodes()->size());
+    EXPECT_EQ(1, wordsGraph->getNeighbours("cad").size());
+    EXPECT_EQ(2, wordsGraph->getNeighbours("cat").size());
+    EXPECT_EQ(1, wordsGraph->getNeighbours("rat").size());
+    EXPECT_EQ("cat", *wordsGraph->getNeighbours("cad").begin());
+    EXPECT_EQ("cat", *wordsGraph->getNeighbours("rat").begin());
+
+    const auto& catNeighbours = wordsGraph->getNeighbours("cat");
+    EXPECT_NE(catNeighbours.end(), catNeighbours.find("cad"));
+    EXPECT_NE(catNeighbours.end(), catNeighbours.find("rat"));
+}
+
+TEST(ProcessingDictionary, 676DifferentWords)
 {
     auto dict = MockDictionary::Create();
     for (char a = 'a'; a <= 'z'; ++a) {
@@ -55,18 +76,18 @@ TEST(AmountOfWords, DifferentWords676)
         }
     }
     auto amountOfAddedWords = std::pow('z' - 'a' + 1, 2);
+    ASSERT_EQ(amountOfAddedWords, dict->size());
 
     auto wordsGraph = WordsGraphFactory::CreateWordsGraph();
     wordsGraph->setData(dict);
 
     auto nodes = wordsGraph->getNodes();
     EXPECT_EQ(amountOfAddedWords, nodes->size());
-    EXPECT_EQ(dict->size(), nodes->size());
 }
 
 // AmountOfWords.DifferentWords17576 takes around 30-50 seconds.
 // With current algorithm of build graph, I decided to disable it.
-TEST(AmountOfWords, DifferentWords17576)
+TEST(ProcessingDictionary, 17576DifferentWords)
 {
     auto dict = MockDictionary::Create();
     for (char a = 'a'; a <= 'z'; ++a) {
@@ -77,33 +98,13 @@ TEST(AmountOfWords, DifferentWords17576)
         }
     }
     auto amountOfAddedWords = std::pow('z' - 'a' + 1, 3);
+    ASSERT_EQ(amountOfAddedWords, dict->size());
 
     auto wordsGraph = WordsGraphFactory::CreateWordsGraph();
     wordsGraph->setData(dict);
 
     auto nodes = wordsGraph->getNodes();
     EXPECT_EQ(amountOfAddedWords, nodes->size());
-    EXPECT_EQ(dict->size(), nodes->size());
-}
-
-TEST(ProcessingDictionary, EmptyDictionary)
-{
-    auto dict = MockDictionary::Create();
-    auto wordsGraph = WordsGraphFactory::CreateWordsGraph();
-    wordsGraph->setData(dict);
-
-    auto nodes = wordsGraph->getNodes();
-    EXPECT_EQ(0, nodes->size());
-}
-
-TEST(ProcessingDictionary, OneWordDictionary)
-{
-    auto dict = MockDictionary::Create("cat");
-    auto wordsGraph = WordsGraphFactory::CreateWordsGraph();
-    wordsGraph->setData(dict);
-
-    EXPECT_EQ(1, wordsGraph->getNodes()->size());
-    EXPECT_EQ(0, wordsGraph->getNeighbours("cat").size());
 }
 
 int main(int argc, char **argv)
